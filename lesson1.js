628db79bf7e09471b5e4d9a1d60f5d64fcdd6ce7
AmirAlakbarli <emirelekberli@gmail.com>
2023-02-23 20:14:49 +0400

    First two lesson codes have been added to repo

---
+++ lesson1.js
@@ -0,0 +1,61 @@
+//* 'a', '\n', '1', '#' -> char
+
+//! Each character in the ASCII table corresponds to a number.  (encoding process)
+//* for example 'A' corresponds to 65 in ASCII table
+
+// let a = 1;
+// if (a == "1") {
+//   console.log("a equal 1");
+// } else {
+//   console.log("a not equal 1");
+// }
+
+//! Code written scratch with variable to sum
+// let a1 = 1;
+// let a2 = 2;
+// let a3 = 3;
+
+// let sum = a1 + a2 + a3;
+// console.log(sum);
+
+//! It is very easy to sum of many number with loop
+//* Kind of loop -> While, do while, for
+
+// let a = 1;
+// while (a == 1) {
+//     console.log("this is true");
+//     break;
+// }
+
+//! increment in while
+// let sum = 0;
+// let init = 0;
+// while (++init <= 10) {
+//   sum += init ;
+// }
+// console.log(sum);
+
+//! example of do-while loop
+// let a = 1;
+// do {
+//   console.log(a);
+// } while (a < 0);
+
+//! example of for loop
+// let sum = 0;
+// for (let num = 0; num <= 10; num++) {
+//   sum += num;
+// }
+// console.log(sum);
+
+//* task
+
+function addOdd(num) {
+  let res = "";
+  for (let i = 1; i < num; i += 2) {
+    res += i + " ";
+  }
+  return res;
+}
+
+console.log(addOdd(20));
---
+++ lesson2.js
@@ -0,0 +1,52 @@
+// GCD -> EBOB
+// LCM -> EKOB
+
+// 24, 18
+// 24 = 2*2*2*3
+// 18 = 2*3*3
+// GCD (24, 18) = 2*3 = 6
+// LCM(24, 18) = 24*3=72
+// LCM = (a*b)/GCD
+// (2*2*2*3)*(2*3*3)/2*3 = 24*3
+
+function GCD(a, b) {
+  // let littleNum = a > b ? b : a; //* if a > b then return b, else return a
+  // let bigNum = a > b ? a : b; //* if a > b then return a, else return b
+  let littleNum, bigNum;
+  if (a > b) {
+    bigNum = a;
+    littleNum = b;
+  } else {
+    bigNum = b;
+    littleNum = a;
+  }
+
+  let res = littleNum;
+
+  while (res > 1 && (bigNum % res != 0 || littleNum % res != 0)) {
+    res--;
+  }
+
+  return res;
+}
+
+function LCMwithGCD(a, b) {
+  return (a * b) / GCD(a, b);
+}
+
+//! ternary operator a > b ? a : b
+function LCM(a, b) {
+  let bigNum = a > b ? a : b;
+  let res = bigNum;
+  for (let i = bigNum; i < a * b; i++) {
+    // if (res % a == 0 && res % b == 0) break;
+    if ((i % a) + (i % b) == 0) {
+      res = i;
+      break;
+    }
+  }
+  return res;
+}
+
+console.log(LCMwithGCD(24, 18));
+console.log(LCM(24, 18));
